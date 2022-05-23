2b478df74858771e6009ecec632e78908a6a709e
Bonginkosi Lukhele <bonginkosi@datawizzards.io>
2022-05-23 06:44:51 +0200

    handle no slides or less than 5

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -14,15 +14,16 @@ import { ReactComponent as Chev } from "../../assets/svg/chevron-right.svg";
  */
 
 function Carousel({ slides }) {
-    const [tempSlide, setTempSlide] = useState(slides);
-    const [dark, setDark] = useState(false);
-    useEffect(() => {
-      const timer = setInterval(() => {
-        next(1);
-      }, 1000 * 15);
-  
-      return () => clearInterval(timer);
-    }, []);
+  const [tempSlide, setTempSlide] = useState(slides);
+  const [dark, setDark] = useState(false);
+  useEffect(() => {
+    if (!slides) return null;
+    const timer = setInterval(() => {
+      next(1);
+    }, 1000 * 15);
+
+    return () => clearInterval(timer);
+  }, []);
 
   // forward/back controls
   function next(n) {
@@ -41,61 +42,81 @@ function Carousel({ slides }) {
   }
 
   return (
-    <div className="slideshow-container fade">
-      <div className="container">
-          <div className="comment"><span>What are you<br/><b>here to do?</b></span></div>
-        {tempSlide.map(({ title, image }, i) => {
-          let slideClass = "";
-          // assign class to slide according to position
-          switch (i) {
-            case 0:
-              slideClass = "one";
-              break;
-            case 1:
-              slideClass = "two";
-              break;
-            case 2:
-              slideClass = "main";
-              break;
-            case 3:
-              slideClass = "three";
-              break;
-            case 4:
-              slideClass = "four";
-              break;
-
-            default:
-              return null;
-          }
-          return (
-            <div key={title + "-" + i} className={`${slideClass} `}>
-              <div className={`inner-container ${i==2&&dark?" dark ":""}`}>
-                <div className="img-container">
-                  <img src={image} alt={title} />
-                </div>
-                {/* text content of the slide */}
-                <div className="card-text">
-                  <h4>{title}</h4>
-                  {/* only show 'start here' on the main slide*/}
-                  {i == 2 ? (
-                    <span onClick={()=>setDark(!dark)} className="start-here">
-                      {" start here "} <Chev width="14px" style={{fontSize:"12px"}} />
-                    </span>
-                  ) : null}
-                </div>
-              </div>
+    <>
+      {tempSlide && tempSlide.length ? (
+        <div className="slideshow-container fade">
+          <div className="container">
+            <div className="comment">
+              <span>
+                What are you
+                <br />
+                <b>here to do?</b>
+              </span>
             </div>
-          );
-        })}
-      </div>
-      {/* slide controls */}
-      <a className="Back" onClick={() => next(-1)}>
-        <Left />
-      </a>
-      <a className="forward" onClick={() => next(1)}>
-        <Right />
-      </a>
-    </div>
+            {tempSlide.map(({ title, image }, i) => {
+              let slideClass = "";
+              // assign class to slide according to position
+              switch (i) {
+                case 0:
+                  slideClass = "one";
+                  break;
+                case 1:
+                  slideClass = "two";
+                  break;
+                case 2:
+                  slideClass = "main";
+                  break;
+                case 3:
+                  slideClass = "three";
+                  break;
+                case 4:
+                  slideClass = "four";
+                  break;
+
+                default:
+                  return null;
+              }
+              return (
+                <div key={title + "-" + i} className={`${slideClass} `}>
+                  <div
+                    className={`inner-container ${
+                      i == 2 && dark ? " dark " : ""
+                    }`}
+                  >
+                    <div className="img-container">
+                      <img src={image} alt={title?title:"slide-"+i} />
+                    </div>
+                    {/* text content of the slide */}
+                    <div className="card-text">
+                      {title?<h4>{title}</h4>:""}
+                      {/* only show 'start here' on the main slide*/}
+                      {i == 2 ? (
+                        <span
+                          onClick={() => setDark(!dark)}
+                          className="start-here"
+                        >
+                          {" start here "}{" "}
+                          <Chev width="14px" style={{ fontSize: "12px" }} />
+                        </span>
+                      ) : null}
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+          {/* slide controls */}
+          <a className="Back" onClick={() => next(-1)}>
+            <Left />
+          </a>
+          <a className="forward" onClick={() => next(1)}>
+            <Right />
+          </a>
+        </div>
+      ) : (
+        <div>No Slides Loaded</div>
+      )}
+    </>
   );
 }
 
